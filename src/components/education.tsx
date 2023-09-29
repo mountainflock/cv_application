670a3593e6e6a6e336a621998acd8cc0c57f47b7
Marina <118333236+mountainflock@users.noreply.github.com.>
2023-09-29 21:51:15 +0300

    Add Education and Work components including interactivity

--- src/components/education.tsx
+++ src/components/education.tsx
@@ -2,19 +2,39 @@ import EditButton from "./editButton";
 import SubmitButton from "./submitButton";
 
 
-const Education = ({school, title, finished, editInfo, isSubmit}) => {
+const Education = ({
+    school, 
+    title, 
+    finished, 
+    isEducationSubmit, 
+    setIsEducationSubmit, 
+    editSchool, 
+    editTitle, 
+    editFinished}) => {
+        
+    if (isEducationSubmit) 
+    return (
+    <div className='section'>
+            <h3>Education <EditButton/></h3>
+        <div>
+            <p>{school}</p>
+            <p>{title}</p>
+            <p>{finished}</p>
+        </div>
+    </div>
+    )
+
 
 return (
     <div className="section">
         <div>
             <h3>Education <EditButton/></h3>
-            <form onSubmit={editInfo} >
-                <p><input placeholder="school" defaultValue={school}/></p>
-                <p><input placeholder="title" defaultValue={title}/></p>
-                <p><input placeholder="finished" type="date" defaultValue={finished}/></p>
+            <form onSubmit={setIsEducationSubmit} >
+                <p><input onChange={editSchool} placeholder="school" defaultValue={school}/></p>
+                <p><input onChange={editTitle} placeholder="title" defaultValue={title}/></p>
+                <p><input onChange={editFinished} placeholder="finished" type="date" defaultValue={finished}/></p>
                 <SubmitButton/>
             </form>
-
         </div>
     </div>
 )
