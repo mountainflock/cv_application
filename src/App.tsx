a4f89e18e758414b11d564589f1b0a49aaf3b8c7
Marina <reddymikks@gmail.noreply.github.com>
2023-09-29 21:51:15 +0300

    Add Education and Work components including interactivity

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
       endDate: ""}});
 
   const [isGeneralSubmit, setIsGeneralSubmit] = useState(false);
-  const [isEducationSibmit, setIsEducationSibmit] = useState(false);
+  const [isEducationSubmit, setIsEducationSibmit] = useState(false);
   const [isWorkSubmit, setIsWorkSubmit] = useState(false);
 
   function handleGeneralSubmit(event) {
@@ -59,29 +59,87 @@ function App() {
         phone: event.currentTarget.value}});
   }
 
-//   function handleEducationChange(e) {
-//     e.preventDefault();
-//     setEducation({
-//       ...education,
-//       school: e.target.value,
-//       title: e.target.value,
-//       finished: e.target.value
-//   });
-//   setIsEducationSubmit(true);
-// }
+  function handleEducationSubmit(event) {
+    event.preventDefault();
+    setIsEducationSibmit(true);
+  }
 
-//   function handleWorkExperienceChange(e) {
-//     e.preventDefault();
-//     setWork({
-//       ...work,
-//       company: e.target.value,
-//       position: e.target.value,
-//       responsibilities: e.target.value,
-//       startDate: e.target.value,
-//       endDate: e.target.value,
-//     });
-//     setIsWorkSubmit(true);
-//   }
+  function saveSchool(event) {
+    event.preventDefault();
+    setPerson({
+      ...person,
+      educaction: {
+        ...person.educaction,
+        school: event.currentTarget.value}});
+  }
+
+  function saveTitle(event) {
+    event.preventDefault();
+    setPerson({
+      ...person,
+      educaction: {
+        ...person.educaction,
+        title: event.currentTarget.value}});
+  }
+
+  function saveFinished(event) {
+    event.preventDefault();
+    setPerson({
+      ...person,
+      educaction: {
+        ...person.educaction,
+        finished: event.currentTarget.value}});
+  }
+
+  function handleWorkSubmit(event) {
+    event.preventDefault();
+    setIsWorkSubmit(true);
+  }
+
+  function saveCompany(event) {
+    event.preventDefault();
+    setPerson({
+      ...person,
+      work: {
+        ...person.work,
+        company: event.currentTarget.value}});
+  }
+
+  function savePosition(event) {
+    event.preventDefault();
+    setPerson({
+      ...person,
+      work: {
+        ...person.work,
+        position: event.currentTarget.value}});
+  }
+
+  function saveResponsibilities(event) {
+    event.preventDefault();
+    setPerson({
+      ...person,
+      work: {
+        ...person.work,
+        responsibilities: event.currentTarget.value}});
+  }
+
+  function saveStartDate(event) {
+    event.preventDefault();
+    setPerson({
+      ...person,
+      work: {
+        ...person.work,
+        startDate: event.currentTarget.value}});
+  }
+
+  function saveEndDate(event) {
+    event.preventDefault();
+    setPerson({
+      ...person,
+      work: {
+        ...person.work,
+        endDate: event.currentTarget.value}});
+  }
 
   return (
     <div className = "app">
@@ -96,23 +154,31 @@ function App() {
       isGeneralSubmit={isGeneralSubmit}/>
 
 
-      {/* <Education
-      school=''
-      title=''
-      finished=''
-      editInfo={handleEducationChange}
-      isSubmit={isEducationSubmit}
+      <Education
+      school={person.educaction.school}
+      title={person.educaction.title}
+      finished={person.educaction.finished}
+      editSchool={saveSchool}
+      editTitle={saveTitle}
+      editFinished={saveFinished}
+      setIsEducationSubmit={handleEducationSubmit}
+      isEducationSubmit={isEducationSubmit}
       />
 
       <WorkExperience
-      company=''
-      position=''
-      responsibilities=''
-      startDate=''
-      endDate=''
-      editInfo={handleWorkExperienceChange}
-      isSubmit={isWorkSubmit}
-      /> */}
+      company={person.work.company}
+      position={person.work.position}
+      responsibilities={person.work.responsibilities}
+      startDate={person.work.startDate}
+      endDate={person.work.endDate}
+      editCompany={saveCompany}
+      editPosition={savePosition}
+      editResponsibilities={saveResponsibilities}
+      editStartDate={saveStartDate}
+      editEndDate={saveEndDate}
+      setIsWorkSubmit={handleWorkSubmit}
+      isWorkSubmit={isWorkSubmit}
+      />
     </div>
 )
   }
--- src/components/work.tsx
+++ src/components/work.tsx
@@ -2,18 +2,45 @@ import EditButton from "./editButton"
 import SubmitButton from "./submitButton" 
 
 
-const WorkExperience = ({company, position, responsibilities, startDate, endDate, editInfo, isSubmit}) => {
+const WorkExperience = ({
+    company, 
+    position, 
+    responsibilities, 
+    startDate, 
+    endDate, 
+    isWorkSubmit,
+    editCompany, 
+    editPosition, 
+    editResponsibilities, 
+    editStartDate, 
+    editEndDate,
+setIsWorkSubmit}) => {
+
+
+    if (isWorkSubmit) 
+    return (
+    <div className='section'>
+            <h3>Work Experience <EditButton/></h3>
+        <div>
+            <p>{company}</p>
+            <p>{position}</p>
+            <p>{responsibilities}</p>
+            <p>{startDate}</p>
+            <p>{endDate}</p>
+        </div>
+    </div>
+    )
 
 return (
     <div className="section">
         <div>
             <h3>Work Experience <EditButton/></h3>
-            <form onSubmit={editInfo} >
-                <p><input placeholder="company" defaultValue={company}/></p>
-                <p><input placeholder="position" defaultValue={position}/></p>
-                <p><textarea placeholder="responsibilities" defaultValue={responsibilities}/></p>
-                <p>from <input type='date' defaultValue={startDate}/></p>
-                <p>to <input type='date' defaultValue={endDate}/></p>
+            <form onSubmit={setIsWorkSubmit} >
+                <p><input onChange={editCompany} placeholder="company" defaultValue={company}/></p>
+                <p><input onChange={editPosition} placeholder="position" defaultValue={position}/></p>
+                <p><textarea onChange={editResponsibilities} placeholder="responsibilities" defaultValue={responsibilities}/></p>
+                <p> <input onChange={editStartDate} type='date' defaultValue={startDate}/></p>
+                <p> <input onChange={editEndDate}type='date' defaultValue={endDate}/></p>
                 <SubmitButton/>
             </form>
         </div>
