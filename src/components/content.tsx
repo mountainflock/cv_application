47958882d44ac3ceef58686cae1f393803e53609
Marina <reddymikks@gmail.noreply.github.com>
2023-10-02 18:59:23 +0300

    Apply appearance changes

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -132,7 +132,7 @@ const Content = () => {
   }
 
   if (isSubmit) return (
-  <div>
+  <div className="content">
   <h3 className="section-title">General Info </h3>
   <Line title={"name"} value={data.general.name}/>
   <Line title={"email"} value={data.general.email}/>
@@ -146,7 +146,10 @@ const Content = () => {
   <h3 className="section-title">Work Experience </h3>
   <Line title={"company"} value={data.work.company}/>
   <Line title={"position"} value={data.work.position}/>
-  <Line title={"responsibilities"} value={data.work.responsibilities}/>
+  <div>
+    <p className="cv-line-title"> responsibilities:</p>
+     <p className="line"> {data.work.responsibilities}</p>
+  </div>
   <Line title={"start date"} value= {data.work.startDate}/>
   <Line title={"end date"} value = {data.work.endDate}/>
   <EditButton onClick={toggleIsSubmit}/>
@@ -154,25 +157,25 @@ const Content = () => {
   )
   
   return (
-<div>
+<div className="content">
 <form onSubmit={toggleIsSubmit}>
-  <h3 className="section-title">General Info </h3>
-    <Input onChange={saveName} title="name" type="text" value={data.general.name}/>
-    <Input onChange={saveEmail} title="e-mail" type="email" value={data.general.email}/>
-    <Input onChange={savePhone} title="phone" type="tel" value={data.general.phone}/>
 
-  <h3 className="section-title">Education </h3>
-    <Input onChange={saveSchool} title="school" type="text" value={data.educaction.school}/>
-    <Input onChange={saveTitleOfStudy} title="title" type="text" value={data.educaction.titleOfStudy}/>
-    <Input onChange={saveFinished} title="finished" type="date" value={data.educaction.finished}/>
+    <h3 className="section-title">General Info </h3>
+      <Input onChange={saveName} title="name" type="text" value={data.general.name}/>
+      <Input onChange={saveEmail} title="e-mail" type="email" value={data.general.email}/>
+      <Input onChange={savePhone} title="phone" type="tel" value={data.general.phone}/>
+    <h3 className="section-title">Education </h3>
+      <Input onChange={saveSchool} title="school" type="text" value={data.educaction.school}/>
+      <Input onChange={saveTitleOfStudy} title="title" type="text" value={data.educaction.titleOfStudy}/>
+      <Input onChange={saveFinished} title="finished" type="date" value={data.educaction.finished}/>
 
-  <h3 className="section-title">Work Experience </h3>
-    <Input onChange={saveCompany} title="company" type="text" value={data.work.company}/>
-    <Input onChange={savePosition} title="position" type="text" value={data.work.position}/>
-    <span className="input-title">responsibilities:</span><p className="input-title"><textarea onChange={saveResponsibilities} value={data.work.responsibilities}/></p>
-    <Input onChange={saveStartDate} title="from" type="date" value={data.work.startDate}/>
-    <Input onChange={saveEndDate} title="to" type="date" value={data.work.endDate}/>
-    <SubmitButton/>
+    <h3 className="section-title">Work Experience </h3>
+      <Input onChange={saveCompany} title="company" type="text" value={data.work.company}/>
+      <Input onChange={savePosition} title="position" type="text" value={data.work.position}/>
+      <span className="input-title">responsibilities:</span><p className="input-title"><textarea onChange={saveResponsibilities} value={data.work.responsibilities}/></p>
+      <Input onChange={saveStartDate} title="from" type="date" value={data.work.startDate}/>
+      <Input onChange={saveEndDate} title="to" type="date" value={data.work.endDate}/>
+      <SubmitButton/>
 </form>
 </div>
   )
--- src/components/line.tsx
+++ src/components/line.tsx
@@ -3,7 +3,7 @@ import "../styles/line.css"
 const Line = ({title, value}) => {
     return (
     <p className="cv-line" >
-       <span className="cv-line-title">{title}:</span> {value}
+       <span className="cv-line-title">{title}:</span> <span className="line">{value}</span>
     </p>
     )
 }
